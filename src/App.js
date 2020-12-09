6523dbd24d21e9d7f69a536dd648d8c5015f04b5
Trevor Mearns <Trevor Mearns>
2020-12-09 16:04:33 +0900

    first commit to new repo

--- src/App.js
+++ src/App.js
@@ -1,85 +1,103 @@
-import React from 'react'
-import Table from './components/Table'
-import UserInput from './components/UserInput'
+import React from 'react';
+import Table from './components/Table';
+import UserInput from './components/UserInput';
 import './App.css';
-import StarWarsPagination from './components/StarWarsPagination'
-
-
+import StarWarsPagination from './components/StarWarsPagination';
 
 // import axios from 'axios'
 // let date = new Date
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
+
     this.state = {
-     activePage: 1,
-     loading: false,
-     character: []
+      activePage: 1,
+      loading: false,
+      character: []
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+    this.handleDataRequest = this.handleDataRequest.bind(this);
   }
- this.handlePageChange = this.handlePageChange.bind(this)
-  this.handleDataRequest = this.handleDataRequest.bind(this)
-}
 
-handlePageChange(pageNumber) {
-  console.log(`pageChanged ${this.activePage}`)
-  this.setState({
-    activePage: pageNumber
-  })
-}
+  handlePageChange = (page) => {
+    console.log('page number selected: ', page);
+    this.setState({
+      activePage: page
+    });
+  };
 
-componentDidMount() {
-this.handlePageChange(this.activePage);
-this.handleDataRequest();  
-}
+  componentDidMount() {
+    // this.handlePageChange(this.activePage);
+    this.handleDataRequest();
+  }
 
-handleDataRequest() {
-  this.setState({
-    loading: true
-  })
-  
-  Promise.all([
-  fetch(`https://swapi.dev/api/people/?page=${this.state.activePage}`).then(people => people.json()),
-  fetch(`https://swapi.dev/api/planets/?page=${this.state.activePage}`).then(planets => planets.json()),
-  fetch(`https://swapi.dev/api/species/?page=${this.state.activePage}`).then(species => species.json())
-])
-.then(response => this.setState({ 
-  loading: false, 
-  character: [...response[0].results ], 
-  planets: [...response[1].results],
-  species: [...response[2].results]
-}))
-  .catch(error => {console.log(error);});
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.activePage !== prevState.activePage) {
+      this.handleDataRequest();
+    }
+  }
 
-}
+  handleDataRequest() {
+    this.setState({
+      loading: true
+    });
 
- 
+    Promise.all([
+      fetch(
+        `https://swapi.dev/api/people/?page=${this.state.activePage}`
+      ).then((people) => people.json()),
+      fetch(
+        `https://swapi.dev/api/planets/?page=${this.state.activePage}`
+      ).then((planets) => planets.json()),
+      fetch(
+        `https://swapi.dev/api/species/?page=${this.state.activePage}`
+      ).then((species) => species.json())
+    ])
+      .then((response) =>
+        this.setState({
+          loading: false,
+          character: [...response[0].results],
+          planets: [...response[1].results],
+          species: [...response[2].results]
+        })
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-
- render() { 
-  return (
-    <div className="App">   
-          <h1 style=
-          {{ 
-            fontSize: '60px', 
+  render() {
+    return (
+      <div className='App'>
+        <h1
+          style={{
+            fontSize: '60px',
             color: 'yellow'
-          }}>Star Wars API</h1>
-      <UserInput />
-      {this.state.loading ? <h1 style={{color: 'yellow'}}>Loading...</h1> : 
-      <Table 
-        character={this.state.character}
-        planets={this.state.planets}
-        species={this.state.species}
-      />}
-      <StarWarsPagination
-      activePage={this.state.activePage}
-      itemsCountPerPage={10}
-      totalItemsCount={82}
-      pageRangeDisplayed={10}
-      onChange={this.handlePageChange} />
-    </div>
-  );}
+          }}
+        >
+          Star Wars API
+        </h1>
+        <UserInput />
+        {this.state.loading ? (
+          <h1 style={{ color: 'yellow' }}>Loading...</h1>
+        ) : (
+          <Table
+            character={this.state.character}
+            planets={this.state.planets}
+            species={this.state.species}
+          />
+        )}
+        <StarWarsPagination
+          //activePage={this.state.activePage}
+          itemsCountPerPage={10}
+          totalItemsCount={82}
+          pageRangeDisplayed={10}
+          handlePageChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
